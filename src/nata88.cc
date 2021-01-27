15851e27954c0321fd923d1f37f5d8172e9adf13
Aleksey Cheusov <vle@gmx.net>
2021-01-28 02:23:14 +0300

    nata88 exits with error when DELETE_INT_FROM_HASH(key)

--- src/nata88.cc
+++ src/nata88.cc
@@ -8,7 +8,7 @@
 
 #define RESERVE_INT(size)
 #define INSERT_INT_INTO_HASH(key, value) *nata88ins(&hash_int, key, NULL) = value;
-#define DELETE_INT_FROM_HASH(key) //nata88del(&hash_int, key, NULL);
+#define DELETE_INT_FROM_HASH(key) printf("nata88del is not implemented yet"); exit(72); //nata88del(&hash_int, key, NULL);
 #define FIND_INT_EXISTING_FROM_HASH(key) if (nata88get(hash_int, key, NULL) == NULL) { printf("error"); exit(1); }
 #define FIND_INT_MISSING_FROM_HASH(key) if (nata88get(hash_int, key, NULL) != NULL) { printf("error"); exit(1); }
 #define FIND_INT_EXISTING_FROM_HASH_COUNT(key, count) if (nata88get(hash_int, key, NULL) != NULL) { count++; }
