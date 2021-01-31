b0a4ccfc2de079acd0a0c5d29025f49c90cd89f2
Aleksey Cheusov <vle@gmx.net>
2021-02-01 01:11:35 +0300

    nata88.cc: code clean-ups

--- src/nata88.cc
+++ src/nata88.cc
@@ -4,15 +4,31 @@
 
 #define __UNCONST(p) ((void *)((char*)NULL+((char*)p-(char*)NULL)))
 
-#define SETUP_INT void *hash_int = NULL;
-
+#define SETUP_INT \
+	void *hash_int = NULL;
 #define RESERVE_INT(size)
-#define INSERT_INT(key, value) *nata88ins(&hash_int, key, NULL) = value;
-#define DELETE_INT(key) printf("nata88del is not implemented yet"); exit(72); //nata88del(&hash_int, key, NULL);
-#define FIND_INT_EXISTING(key) if (nata88get(hash_int, key, NULL) == NULL) { printf("error"); exit(1); }
-#define FIND_INT_MISSING(key) if (nata88get(hash_int, key, NULL) != NULL) { printf("error"); exit(1); }
-#define FIND_INT_EXISTING_COUNT(key, count) if (nata88get(hash_int, key, NULL) != NULL) { count++; }
-#define CHECK_INT_ITERATOR_VALUE(iterator, value) printf("iteration for nata88 is not implemented yet"); exit(73);
+#define INSERT_INT(key, value) \
+	*nata88ins(&hash_int, key, NULL) = value;
+#define DELETE_INT(key) \
+	printf("nata88del is not implemented yet"); \
+	exit(72); //nata88del(&hash_int, key, NULL);
+#define FIND_INT_EXISTING(key) \
+	if (nata88get(hash_int, key, NULL) == NULL) { \
+		printf("error"); \
+		exit(1); \
+	}
+#define FIND_INT_MISSING(key) \
+	if (nata88get(hash_int, key, NULL) != NULL) { \
+		printf("error"); \
+		exit(1); \
+	}
+#define FIND_INT_EXISTING_COUNT(key, count) \
+	if (nata88get(hash_int, key, NULL) != NULL) { \
+		count++; \
+	}
+#define CHECK_INT_ITERATOR_VALUE(iterator, value) \
+	printf("iteration for nata88 is not implemented yet"); \
+	exit(73);
 #define ITERATE_HASH(key)
 
 #define LOAD_FACTOR_INT_HASH(hash) (0.0f)
